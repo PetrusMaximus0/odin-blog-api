a39c5f5e61f69f261735c1b4f0ecc49682c04149
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-05-27 12:41:03 +0200

    feat: Add an endpoint for creating a new user and for validating a received user token.
    - Add the creation of a route for creating a new user.
    - Add a new validate token route.
    - Change the login route response status code for failure to 401.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,26 +2,27 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 exports.login_index = function (req, res, next) {
 	res.redirect('/users/login');
 };
 
-exports.login_get = function (req, res, next) {
+exports.login_GET = function (req, res, next) {
 	res.send(`Render the login form for the user`);
 };
 
-exports.login_post = asyncHandler(async function (req, res, next) {
+exports.login_POST = asyncHandler(async function (req, res, next) {
 	// The form should bring in a username and a password.
 	const user = await User.findOne({ username: req.body.username }).exec();
 	if (!user) {
-		res.status(403).send('No user with this username.');
+		res.status(401).send({ error: 'No user with this username.' });
 		return;
 	}
 	const result = await bcrypt.compare(req.body.password, user.password);
 	if (!result) {
 		//
-		res.status(403).send('Wrong password.');
+		res.status(401).send({ error: 'Wrong password' });
 		return;
 	}
 	// User exists and password is correct. Obtain token and send to client.
@@ -43,3 +44,75 @@ exports.login_post = asyncHandler(async function (req, res, next) {
 		}
 	);
 });
+
+exports.validateToken_GET = asyncHandler(async function (req, res, next) {
+	const bearerHeader = req.headers['authorization'];
+	if (typeof bearerHeader !== 'undefined') {
+		const bearerToken = bearerHeader.split(' ')[1];
+		req.token = bearerToken;
+	} else {
+		res.status(403).send('Token verification failed.');
+	}
+	// Verify the local token
+	jwt.verify(
+		req.token,
+		process.env.SECRET,
+		{ expiresIn: '120s' },
+		(err, authData) => {
+			if (err) {
+				res.status(403).send(err);
+			} else {
+				req.authData = authData;
+				res.json(authData);
+			}
+		}
+	);
+});
+
+exports.newuser_POST = [
+	// Validate and Sanitize
+	body('username')
+		.trim()
+		.isAlphanumeric()
+		.withMessage(
+			'Usernames must consist of single words composed of alphanumeric characters'
+		)
+		.isLength({ min: 1, max: 100 })
+		.escape(),
+	body('password').trim().isLength({ min: 1, max: 100 }).escape(),
+	asyncHandler(async function (req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// There are errors, render the form again with errors and sanitized values
+			res.send({
+				user: {
+					username: req.body.username,
+					password: '',
+				},
+				errors: errors.array(),
+			});
+		}
+
+		// Check if the user exists already.
+		const user = await User.findOne({ username: req.body.username }).exec();
+		if (user) {
+			res.status(403).send({
+				error: 'This username is not available',
+			});
+			return;
+		}
+		// Create a new user and save to the database.
+		const newUser = new User({
+			username: req.body.username,
+			password: await bcrypt.hash(req.body.password, 10),
+			isAdmin: false,
+			posts: [],
+		});
+
+		await newUser.save();
+
+		// send success message
+		res.status(201).send({ message: 'success' });
+		return;
+	}),
+];
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,12 @@ const userController = require('../controllers/userController');
 //
 router.get('/', userController.login_index);
 //
-router.get('/login', userController.login_get);
+router.get('/login', userController.login_GET);
 //
-router.post('/login', userController.login_post);
+router.post('/login', userController.login_POST);
+// Authenticate the user token,
+router.get('/validateToken', userController.validateToken_GET);
+//
+router.post('/new', userController.newuser_POST);
 
 module.exports = router;
