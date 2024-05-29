f7f8de30f3e90ad8b9c86cf4304b42972734fa3c
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-05-29 20:13:41 +0200

    feat: blogpost schema field 'text' max length increase to 10 000

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
 	author: { type: String, maxLength: 100, minLenght: 1, required: true },
 	title: { type: String, maxLenght: 100, minLenght: 1, required: true },
-	text: { type: String, maxLenght: 1000, minLenght: 1, required: true },
+	text: { type: String, maxLenght: 10000, minLenght: 1, required: true },
 	description: { type: String, maxLenght: 200, minLenght: 1, required: true },
 	date: { type: Date, required: true },
 	timeToRead: { type: String, maxLength: 10, minLength: 1, required: true },
